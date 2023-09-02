d53a1880740cc1ce26c354440e5474720422e64f
neila <neil.agarwal@shoplc.com>
2023-09-02 13:51:19 -0500

    Implemented product data aggregation cloud function

--- run_aggregate_reviews/index.js
+++ run_aggregate_reviews/index.js
@@ -1,41 +1,101 @@
 const express = require('express');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { Firestore } = require('@google-cloud/firestore');
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const {Firestore} = require('@google-cloud/firestore');
+const functions = require("@google-cloud/functions-framework");
+const axios = require("axios");
 
-const app = express();
 const secretClient = new SecretManagerServiceClient();
 const firestore = new Firestore();
 
-app.get('/', async (req, res) => {
+async function getAxessoAPIKey() {
 	// Fetch the API key from Secret Manager
 	const [version] = await secretClient.accessSecretVersion({
-		name: 'projects/YOUR_PROJECT_ID/secrets/amazon-api-key/versions/latest'
+		name: 'projects/shoplc-amazon-reviews/secrets/axesso-api-key/versions/latest'
 	});
-	const apiKey = version.payload.data.toString();
+	return version.payload.data.toString()
+}
 
-	// Fetch ASIN numbers from Firestore
-	const asinSnapshot = await firestore.collection('asinNumbers').get();
-	const asinCodes = asinSnapshot.docs.map(doc => doc.data().asin);
+/**
+ * Gets next available reviews page number based on already queried reviews pages.
+ *
+ * @return {Promise<string>}
+ */
+async function getNextPageNumber(asin) {
+	const asinDoc = firestore.collection("products").doc(asin)
 
-	// Fetch reviews for ASIN numbers from Amazon API (Simulated here)
-	// Replace this with your actual API call logic
-	const reviews = asinCodes.map(asin => ({
-		asin,
-		review: 'Sample review for ' + asin
-	}));
+	const doc = await asinDoc.get();
 
-	// Write reviews to a different Firestore collection
-	const batch = firestore.batch();
-	reviews.forEach((review, index) => {
-		const docRef = firestore.collection('asinReviews').doc(index.toString());
-		batch.set(docRef, review);
+	const docData = doc.data()
+
+	let checkedPages = docData.checkedPages || [];
+
+	let maxPage = docData.lastPage || Infinity;
+
+	let pageNumber = 1;
+	while (checkedPages.includes(pageNumber)) {
+		pageNumber++;
+	}
+
+	if (pageNumber > maxPage) {
+		return '1';
+	}
+
+
+}
+
+async function getReviewsAxesso(asin) {
+	const nextPageNumber = getNextPageNumber(asin);
+
+	const options = {
+		method: 'GET',
+		url: 'https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-seller-products',
+		params: {
+			domainCode: 'com',
+			asin,
+			sortBy: 'recent',
+			filters: "reviewerType=all_reviews;filterByStar=one_star",
+			page: nextPageNumber
+		},
+		headers: {
+			'X-RapidAPI-Key': apiKey,
+			'X-RapidAPI-Host': 'axesso-axesso-amazon-data-service-v1.p.rapidapi.com'
+		}
+	};
+
+	const response = await axios.request(options);
+
+	return response.data
+}
+
+
+async function getProductByPriority() {
+	const productsRef = firestore.collection('products');
+
+	// Query Firestore for the document with the highest "priority" value
+	const snapshot = await productsRef.orderBy('priority', 'desc').limit(1).get();
+
+	if (snapshot.empty) {
+		console.log('No matching documents.');
+		return;
+	}
+
+	// Extract the "asin" parameter from the document
+	let asin;
+	snapshot.forEach(doc => {
+		console.log(doc.id, '=>', doc.data());
+		asin = doc.data().asin;
 	});
-	await batch.commit();
 
-	res.status(200).send('Reviews fetched and written to Firestore.');
-});
+	return asin
+}
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+functions.http('aggregate_reviews', async (req, res) => {
+	// Whether to perform the action in overwrite mode
+	const apiKey = await getAxessoAPIKey();
+
+	const asin = await getProductByPriority();
+
+
+
+	res.status(200).send('Data fetched and written to Firestore.');
 });
--- run_collect_asin_codes/index.js
+++ run_collect_asin_codes/index.js
@@ -1,20 +1,89 @@
-const express = require('express');
 const axios = require('axios');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { Firestore } = require('@google-cloud/firestore');
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const {Firestore} = require('@google-cloud/firestore');
+const functions = require('@google-cloud/functions-framework');
 
-const app = express();
 const secretClient = new SecretManagerServiceClient();
 const firestore = new Firestore();
 
-async function getProductsAxesso(apiKey) {
+const sellerID = 'ADZH7GRDFE99Y';
+
+
+/**
+ * Marks a page that has no products as invalid.
+ *
+ * @param pageNumber page number to mark as invalid
+ * @return {Promise<void>}
+ */
+async function markInvalidPageNumber(pageNumber) {
+	const docRef = firestore.collection('info').doc('asinCodes');
+
+	await docRef.update({
+		invalidPages: Firestore.FieldValue.arrayUnion(pageNumber)
+	});
+}
+
+/**
+ * Retrieves asinCode runner info
+ *
+ * @return {Promise<Object>}
+ */
+async function getRunnerInfo() {
+	const docRef = firestore.collection('info').doc('asinCodes');
+	const doc = await docRef.get();
+
+	if (doc.exists) {
+		return doc.data();
+	} else {
+		return {}
+	}
+}
+
+/**
+ * Updates the asinCode runner info.
+ *
+ * @param param Update query to modify runner info with.
+ * @return {Promise<void>}
+ */
+async function updateRunnerInfo(param) {
+	const docRef = firestore.collection('info').doc('asinCodes');
+
+	// Update the checkedPages field in Firestore
+	await docRef.set(param, { merge: true });
+}
+
+/**
+ * Gets next available page number based on already queried pages.
+ *
+ * @return {Promise<string>}
+ */
+async function getNextPageNumber() {
+	const runnerInfo = await getRunnerInfo();
+
+	let checkedPages = runnerInfo.checkedPages || [];
+
+	let pageNumber = 1;
+	while (checkedPages.includes(pageNumber)) {
+		pageNumber++;
+	}
+
+	await updateRunnerInfo({
+		checkedPages: Firestore.FieldValue.arrayUnion(pageNumber)
+	})
+
+	return pageNumber.toString();
+}
+
+async function getProductDataAxesso(apiKey, overwrite = false, pageNumber) {
+	const nextPageNumber = pageNumber ? pageNumber : await getNextPageNumber();
+
 	const options = {
 		method: 'GET',
 		url: 'https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-seller-products',
 		params: {
 			domainCode: 'com',
-			sellerId: 'ADZH7GRDFE99Y',
-			page: '1'
+			sellerId: sellerID,
+			page: nextPageNumber
 		},
 		headers: {
 			'X-RapidAPI-Key': apiKey,
@@ -24,31 +93,66 @@ async function getProductsAxesso(apiKey) {
 
 	const response = await axios.request(options);
 
-	const asinCodes = response.data
+	return response.data
 }
 
-app.get('/', async (req, res) => {
+async function processProductData(productData, overwrite = false, priority=1) {
+	if (productData.numberOfProducts <= 0) {
+		await markInvalidPageNumber(productData.currentPage)
+		return;
+	}
+
+	const products = productData.searchProductDetails;
+
+	// Initialize batch
+	const batch = firestore.batch();
+
+	// Prepare all document references and operations in the batch
+	const productPromises = products.map(async (product) => {
+		const asin = product.asin;
+		const productDocRef = firestore.collection('products').doc(asin);
+
+		if (overwrite) {
+			// Add set operation to batch
+			batch.set(productDocRef, {...product, priority, processed: false});
+		} else {
+			const productDoc = await productDocRef.get();
+
+			if (!productDoc.exists) {
+				// Add set operation to batch if the document doesn't exist
+				batch.set(productDocRef, {...product, priority, processed: false});
+			}
+		}
+	});
+
+	// Wait for all promises to complete
+	await Promise.all(productPromises);
+
+	// Commit the batch
+	await batch.commit();
+
+}
+
+async function getAxessoAPIKey() {
 	// Fetch the API key from Secret Manager
 	const [version] = await secretClient.accessSecretVersion({
 		name: 'projects/shoplc-amazon-reviews/secrets/axesso-api-key/versions/latest'
 	});
-	const apiKey = version.payload.data.toString();
+	return version.payload.data.toString()
+}
+
+functions.http('collect_asin_codes', async (req, res) => {
+	// Whether to perform the action in overwrite mode
+	const overwrite = req.body.packageoverwrite || false;
+	const pageNumber = req.body.pageNumber;
+	const priority = req.body.priority;
+
+	const apiKey = await getAxessoAPIKey();
 
 	// Fetch data from Amazon API
-	const asinNumbers = await getProductsAxesso(apiKey);
+	const productData = await getProductDataAxesso(apiKey, overwrite, pageNumber);
 
-	// Write data to Firestore
-	const batch = firestore.batch();
-	asinNumbers.forEach((asin, index) => {
-		const docRef = firestore.collection('asinNumbers').doc(index.toString());
-		batch.set(docRef, { asin });
-	});
-	await batch.commit();
+	await processProductData(productData, overwrite, priority);
 
 	res.status(200).send('Data fetched and written to Firestore.');
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
 });
